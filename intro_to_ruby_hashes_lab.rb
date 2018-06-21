fd0b790204d0d87ff8accb8f2f41fe9b8f622872
Michael Blandford <mdblandford@gmail.com>
2018-06-21 15:42:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,11 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces =>4, :names => {}, :rent_in_dollars => {}}}
+	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}}}
 end
 
 def monopoly_with_third_tier
